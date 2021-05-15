6dd5f03a508671ec0d059347859408c23dde22c5
Sarma <mmangala@mgc.mentorg.com>
2021-05-15 16:20:04 +0530

    added logging statements

--- covid-vaccine-finder.py
+++ covid-vaccine-finder.py
@@ -91,6 +91,9 @@ class Operations:
                     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) \
                                            AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'},
                 expected_return_code=200)
+        else:
+            if self.args.verbose:
+                print("No telegram tokens found!")
 
     # Method to send whatsapp msg
     def do_whatsapp(self, d_data, token, w_id):
