f169f54c775dd302bc03bf3f2912d17e8863e817
Sarma <mmangala@mgc.mentorg.com>
2021-05-18 00:49:13 +0530

    Adding batch file to run in windows seamlessly for every 10 min

---
+++ run.bat
@@ -0,0 +1,7 @@
+git clone https://github.com/Maheswarasarma/COVID-19-Vaccine-Slot-Finder.git
+cd COVID-19-Vaccine-Slot-Finder.git
+pip install -r requirements.txt
+:loop
+python covid-vaccine-finder.py --age <age> --pincode <pincode> --days <no.of.days> --email <email_id>
+PING localhost -n 1200 >NUL
+goto loop
