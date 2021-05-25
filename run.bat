8f24af2c5876b094127590f6bb896d26e98ae02c
Maheswarasarma <m.maheswarasarma@gmail.com>
2021-05-25 15:55:20 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,5 @@
-git clone https://github.com/Maheswarasarma/COVID-19-Vaccine-Slot-Finder.git
-cd COVID-19-Vaccine-Slot-Finder.git
 pip install -r requirements.txt
+cd COVID-19-Vaccine-Slot-Finder
 :loop
 python covid-vaccine-finder.py --age <age> --pincode <pincode> --days <no.of.days> --email <email_id>
 PING localhost -n 600 >NUL
