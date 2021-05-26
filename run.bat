0019d4569251c89520a47c9b134996ea59d305f4
Maheswarasarma <m.maheswarasarma@gmail.com>
2021-05-27 00:30:11 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,3 @@
-pip install -r requirements.txt
-cd COVID-19-Vaccine-Slot-Finder
 :loop
 python covid-vaccine-finder.py --age <age> --pincode <pincode> --weeks <no.of.weeks> --dose <1|2> --email <email_id> --token <telegram_token> --chat <telegram_chat>
 PING localhost -n 600 >NUL
