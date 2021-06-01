406f2f657de030608ad9562cfbeba804eebf28e1
Maheswarasarma <m.maheswarasarma@gmail.com>
2021-06-01 16:00:24 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 :loop
 python covid-vaccine-finder.py --age <age> --pincode <pincode> --weeks <no.of.weeks> --dose <1|2> --email <email_id> --token <telegram_token> --chat <telegram_chat>
-PING localhost -n 600 >NUL
+timeout /t 600
 goto loop
