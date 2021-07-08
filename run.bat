592b0cb0f88679963b0383df5313772e25885a8a
Maheswarasarma <m.maheswarasarma@gmail.com>
2021-07-08 12:50:24 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 :loop
-python covid-vaccine-finder.py --age <age> --pincode <pincode> --weeks <no.of.weeks> --dose <1|2> --vaccine <covishield|covaxin> --email <email_id> --token <telegram_token> --chat <telegram_chat>
+python covid-vaccine-finder.py --age <age> --pincode <pincode> --weeks <no.of.weeks> --dose <1|2> --vaccine <covishield|covaxin> --price <Free|Paid>--email <email_id> --token <telegram_token> --chat <telegram_chat>
 timeout /t 600
 goto loop
